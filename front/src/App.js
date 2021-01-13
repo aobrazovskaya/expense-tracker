f84441a73942e682cf88605edd84282970401574
aobrazovskaya <anna.obrazovskaya@gmail.com>
2021-01-13 17:18:08 +0300

    Добавляет иконки категорий

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.scss';
-import Categories from './components/Categories/Categories'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Categories from './components/Categories/Categories';
 
 function App() {
   return (
-    <div>
-      <Categories />
+    <div className="app">
+      <BrowserRouter>
+        <div className="app-header">
+          <Header />
+        </div>
+        <div className="app-wrapper">
+          <div className="app-content">
+            <div>
+              <Route exact path="/" component={Main}/>
+              <Route path="/categories" component={Categories}/>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './styles/style.scss';
+import App from './App';
+
+const svgModules = require.context('./asset/icons', false, /\.svg$/);
+svgModules.keys().forEach(svgModules);
 
 ReactDOM.render(
 	<React.StrictMode>
