e8f443dd3accccbfedf49d2bd74efa8ef25ab8e5
Anna <50133132+aobrazovskaya@users.noreply.github.com>
2021-02-19 17:27:50 +0300

    /

--- front/src/App.js
+++ front/src/App.js
@@ -18,7 +18,6 @@ function App() {
           </Route>
           <Route path="/categories">
             <AppWrapper>
-              <Categories />
             </AppWrapper>
           </Route>
           <Route component={ NotFoundPage } />
