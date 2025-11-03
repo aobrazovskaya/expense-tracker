df893fc21ddfa0b345d197640ee06ca190906b2e
aobrazovskaya <anna.obrazovskaya@gmail.com>
2025-11-03 12:56:44 +0200

    add Logo component

---
+++ app/ui/logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <header className="bg-primary flexshrink-0 items-center px-3 py-2 md:px-6 md:py-4">
+      <img src={'./logo-light.png'} className="h-6 w-22 md:h-12 md:w-44" />
+    </header>
+  );
+}
